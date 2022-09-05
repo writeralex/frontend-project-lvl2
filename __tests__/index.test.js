27f511c44c5bfa94bae2fc44da2ffd01b2bc9cb1
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-09-05 17:49:23 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,33 +6,33 @@ test('stylish json files', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
 
-  expect(engineDiff(filepath1, filepath2)).toBe(fs.readFileSync('./__fixtures__/stylishResult.txt', 'utf-8'));
+  expect(engineDiff(filepath1, filepath2)).toEqual(fs.readFileSync('./__fixtures__/stylishResult.txt', 'utf-8'));
 });
 
 test('stylish yaml files', () => {
   const filepath1 = 'file1.yaml';
   const filepath2 = 'file2.yaml';
 
-  expect(engineDiff(filepath1, filepath2)).toBe(fs.readFileSync('./__fixtures__/stylishResult.txt', 'utf-8'));
+  expect(engineDiff(filepath1, filepath2)).toEqual(fs.readFileSync('./__fixtures__/stylishResult.txt', 'utf-8'));
 });
 
 test('plain json files', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
 
-  expect(engineDiff(filepath1, filepath2, 'plain')).toBe(fs.readFileSync('./__fixtures__/plainResult.txt', 'utf-8'));
+  expect(engineDiff(filepath1, filepath2, 'plain')).toEqual(fs.readFileSync('./__fixtures__/plainResult.txt', 'utf-8'));
 });
 
 test('plain yaml files', () => {
   const filepath1 = 'file1.yaml';
   const filepath2 = 'file2.yaml';
 
-  expect(engineDiff(filepath1, filepath2, 'plain')).toBe(fs.readFileSync('./__fixtures__/plainResult.txt', 'utf-8'));
+  expect(engineDiff(filepath1, filepath2, 'plain')).toEqual(fs.readFileSync('./__fixtures__/plainResult.txt', 'utf-8'));
 });
 
 test('json files', () => {
   const filepath1 = 'file1.yaml';
   const filepath2 = 'file2.yaml';
 
-  expect(engineDiff(filepath1, filepath2, 'json')).toBe(fs.readFileSync('./__fixtures__/jsonResult.txt', 'utf-8'));
+  expect(engineDiff(filepath1, filepath2, 'json')).toEqual(fs.readFileSync('./__fixtures__/jsonResult.txt', 'utf-8'));
 });
