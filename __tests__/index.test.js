28cf497f57d12c62e102397a9343bb966226867a
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-11-21 10:17:44 +0300

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,13 @@
 import fs from 'fs';
 import { expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import engineDiff from '../src/index.js';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
